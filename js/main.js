2f592906c7b2dae60310ab7de5333bae5a94da25
Nguyen Anh Viet <van123872000@gmail.com>
2024-04-02 20:49:42 +0700

    finish admin

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+$('table').DataTable( {
+    responsive: true,
+    order: [],
+    language: {
+      "decimal":        "",
+      "emptyTable":     "Không có dữ liệu",
+      "info":           "Hiển thị từ dòng _START_ tới dòng _END_ của _TOTAL_ dòng",
+      "infoEmpty":      "Hiển thị từ dòng 0 tới dòng 0 của 0 dòng",
+      "infoFiltered":   "5",
+      "infoPostFix":    "",
+      "thousands":      ",",
+      "lengthMenu":     "Hiển thị _MENU_ dòng",
+      "loadingRecords": "Đang tải...",
+      "processing":     "",
+      "search":         "Tìm kiếm:",
+      "zeroRecords":    "Dữ liệu không tìm thấy",
+      "paginate": {
+          "first":      "Đầu tiên",
+          "last":       "Cuối cùng",
+          "next":       "Sau",
+          "previous":   "Trước"
+      },
+      "aria": {
+          "orderable":  "Sắp xếp tăng dần theo cột này",
+          "orderableReverse": "Sắp xếp giảm dần theo cột này"
+      }
+    }
+} );
